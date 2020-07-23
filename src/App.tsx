88dc55a48b3408eacf6b544e840ab66f27336e4e
Viet-Nhien Tran Duc <vtranduc@ryerson.ca>
2020-07-22 23:55:54 -0400

    added rough navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./sass/main.scss";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./view/components/NavBar";
+import Footer from "./view/components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Have configured continuous deployed
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <NavBar />
+        <div className="main"></div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/view/components/NavBar/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavBar() {
+  return <div className="navbar">abc</div>;
+}
