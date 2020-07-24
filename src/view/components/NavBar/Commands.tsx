fc30feb8ded0bce526a29c8fa90a242c7413d5ce
Viet-Nhien Tran Duc <vtranduc@ryerson.ca>
2020-07-23 22:21:26 -0400

    better design for navbar buttons

--- src/view/components/NavBar/Commands.tsx
+++ src/view/components/NavBar/Commands.tsx
@@ -1,23 +1,36 @@
 import React from "react";
+import { IconType } from "react-icons";
 import { FaUserAlt } from "react-icons/fa";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { TiChevronRightOutline } from "react-icons/ti";
 
+interface INavBarCommand {
+  label: string;
+  icon: IconType;
+  onClick: () => void;
+}
+
 export default function Commands() {
-  return (
-    <div className="navbar-commands">
-      <div className="navbar-command">
-        <FaUserAlt className="navbar-command-icon" size={"1.5em"} />
-        <label>Login</label>
-      </div>
-      <div className="navbar-command">
-        <RiShoppingCart2Line className="navbar-command-icon" size={"1.5em"} />
-        <label>Cart</label>
-      </div>
-      <div className="navbar-command">
-        <TiChevronRightOutline className="navbar-command-icon" size={"1.5em"} />
-        <label>Request</label>
-      </div>
-    </div>
-  );
+  const commands: INavBarCommand[] = [
+    { label: "Login", icon: FaUserAlt, onClick: () => {} },
+    { label: "Cart", icon: RiShoppingCart2Line, onClick: () => {} },
+    { label: "Request", icon: TiChevronRightOutline, onClick: () => {} },
+  ];
+
+  function getCommandButtons(commands: INavBarCommand[]) {
+    return commands.map((command, index) => {
+      const Icon = command.icon;
+      return (
+        <div
+          key={`navbar-command-${index}-${command.label}`}
+          className="navbar-command"
+        >
+          <Icon className="navbar-command-icon" size={"1.5em"} />
+          <label>{command.label}</label>
+        </div>
+      );
+    });
+  }
+
+  return <div className="navbar-commands">{getCommandButtons(commands)}</div>;
 }
