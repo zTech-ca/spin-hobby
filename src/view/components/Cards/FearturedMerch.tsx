9c2819419c621845993f24ecb2fad62cfd482f88
Viet-Nhien Tran Duc <vtranduc@ryerson.ca>
2020-08-06 04:32:04 -0400

    basic frame for featured merch card

--- src/view/components/Cards/FearturedMerch.tsx
+++ src/view/components/Cards/FearturedMerch.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+const img = "https://static.zerochan.net/Toujou.Nozomi.full.2710162.png";
+const title = "nontan nontan nontan nontan";
+const description = `Love Live![a] School Idol Project is a Japanese multimedia project co-developed by ASCII Media Works' Dengeki G's Magazine, music label Lantis, and animation studio Sunrise. The project revolves around a group of nine schoolgirl friends who become idols in order to save their school from shutting down. It launched in the August 2010 issue of Dengeki G's Magazine, and went on to produce music CDs, anime music videos, two manga adaptations, and video games.
+
+A 13-episode anime television series produced by Sunrise, directed by Takahiko Kyōgoku, and written by Jukki Hanada aired in Japan between January and March 2013, with a second season airing between April and June 2014. Both anime series and film are licensed in North America, the United Kingdom, Australia and New Zealand by NIS America, MVM Entertainment and Madman Entertainment, respectively. An animated film titled Love Live! The School Idol Movie was distributed by Shochiku and released in June 2015. A follow-up project focusing on a new set of idols, titled Love Live! Sunshine!!, launched in 2015.`;
+
+const price = 50;
+
 export function FeaturedMerch() {
-  return <div>this is featured</div>;
+  return (
+    <div className="cards-featured-merch">
+      <img src={img} alt={img} />
+      <div className="cards-featured-merch-details">
+        <h2>{title}</h2>
+        <p className="cards-featured-merch-description">{description}</p>
+        <div className="cards-featured-merch-price">${price} CAD</div>
+      </div>
+    </div>
+  );
 }
--- src/view/pages/Home/Merchandise/index.tsx
+++ src/view/pages/Home/Merchandise/index.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import { FeaturedMerch } from "../../../components/Cards";
 
 export default function Merchandise() {
-  return <FeaturedMerch />;
+  return (
+    <>
+      <FeaturedMerch />
+    </>
+  );
 }
