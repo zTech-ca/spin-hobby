4934b46a0fc0d621a52d7d6e24823452130d445d
crocka <chino123anc@hotmail.com>
2022-07-09 11:39:56 -0400

    Implement Paypal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./sass/main.scss";
 import "react-phone-number-input/style.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./view/components/NavBar";
 import Footer from "./view/components/Footer";
 import Modal from "./view/components/Modal";
 import Home from "./view/pages/Home";
+import Cart from "./view/pages/Cart";
+import CheckOut from "./view/pages/CheckOut";
+import Search from "./view/pages/Search";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<CheckOut />} />
+          <Route path='/search' element={<Search />} />
+        </Routes>
         <Modal />
-        <div className="main">
+        {/* <div className="main">
           <Home />
-        </div>
+        </div> */}
         <Footer />
       </div>
     </BrowserRouter>
--- src/saga/index.ts
+++ src/saga/index.ts
@@ -1,7 +1,9 @@
 import { all } from "redux-saga/effects";
 import { currencySaga } from "./currencySaga";
 import { userSaga } from "./userSaga";
+import { cartSaga } from "./cartSaga";
+import { searchSaga } from "./searchSaga";
 
 export default function* saga() {
-  yield all([currencySaga(), userSaga()]);
+  yield all([currencySaga(), userSaga(), cartSaga(), searchSaga()]);
 }
