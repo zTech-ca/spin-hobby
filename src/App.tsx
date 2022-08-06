4a7480a01f32d3b4c844f2953133c32ca57350ea
vtranduc <51380418+vtranduc@users.noreply.github.com>
2022-08-06 13:34:34 -0400

    Disabled the master branch to tell people it is under maintenance and inventory change for animethon

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import "./sass/main.scss";
 import "react-phone-number-input/style.css";
-import { BrowserRouter } from "react-router-dom";
-import NavBar from "./view/components/NavBar";
-import Footer from "./view/components/Footer";
-import Modal from "./view/components/Modal";
-import Home from "./view/pages/Home";
+//import { BrowserRouter } from "react-router-dom";
+//import NavBar from "./view/components/NavBar";
+//import Footer from "./view/components/Footer";
+//import Modal from "./view/components/Modal";
+//import Home from "./view/pages/Home";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <NavBar />
-        <Modal />
-        <div className="main">
-          <Home />
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+  return <div>Spin Hobby is currently undergoing maintenance and inventory change as we are attending Animethon event from Aug 5th-7th, 2022. We're sorry for incovenience. We will be back towards the end of the event</div>
+  
+  //return (
+    //<BrowserRouter>
+     // <div className="app">
+       // <NavBar />
+        //<Modal />
+        //<div className="main">
+         // <Home />
+        //</div>
+        //<Footer />
+      //</div>
+    //</BrowserRouter>
+  //);
 }
 
 export default App;
