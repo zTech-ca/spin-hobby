23b6a33185f71ada9fa9fcaefc546e577183ecd0
Viet Nhien Tran Duc <vtranduc@adobe.com>
2023-03-12 05:31:41 -0600

    maintenance image homepage

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,114 @@ import "react-phone-number-input/style.css";
 
 function App() {
   return (
-    <>
-      <p>
-        Spin Hobby's design and dev teams are working to bring new tone to the
-        website. We expect to be back by the beginning of March 2023. We
-        apologize for inconvenience.
-      </p>
-      <p>Follow us on facebook to keep updated!</p>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        className="tmp-background-img"
+        style={{
+          objectFit: "cover",
+          height: "100%",
+          maxWidth: "50%",
+        }}
+        alt="mascot"
+        src="assets/under development.png"
+      />
 
-      <a href="https://www.facebook.com/profile.php?id=100084221636045">
-        <div style={{ width: "200px" }}>
+      <div>
+        <p>
+          Spin Hobby's design and dev teams are working to bring new tone to the
+          website. We expect to be back in April of 2023. We apologize for
+          inconvenience.
+        </p>
+        <p>Follow us on facebook to keep updated!</p>
+
+        <a href="https://www.facebook.com/profile.php?id=100084221636045">
+          <div style={{ width: "200px" }}>
+            <img
+              style={{ height: "auto", maxWidth: "100%" }}
+              src="/logo/external/facebook/FindUs-FB-RGB-1067.png"
+              alt="find us on facebook"
+            />
+          </div>
+        </a>
+        <div style={{ width: "100px" }}>
           <img
             style={{ height: "auto", maxWidth: "100%" }}
-            src="/logo/external/facebook/FindUs-FB-RGB-1067.png"
+            src="/QR/qrcode.svg"
             alt="find us on facebook"
           />
         </div>
-      </a>
-      <div style={{ width: "100px" }}>
-        <img
-          style={{ height: "auto", maxWidth: "100%" }}
-          src="/QR/qrcode.svg"
-          alt="find us on facebook"
-        />
+        <div>
+          Here are the list of confirmed events this year (Yes, there could be
+          more!).
+          <ul>
+            <li>March 12th, 2023 - Edmonton Collector Con</li>
+            <li>April 27th-30th, 2023 - Calgary Expo</li>
+            <li>May 6th-7th, 2023 - Fort Fan Fest</li>
+            <li>Animethon 14th-16th, 2023 - Otafest</li>
+            <li>July 14th-16th, 2023 - Animethon</li>
+          </ul>
+        </div>
+        <p>
+          For all inquiries, please feel free to message on facebook page or
+          email us at info@ztech.ca
+        </p>
+        <p>We are looking forward to big launch this year of 2023!</p>
       </div>
-      <p>
-        For all inquiries, please feel free to message on facebook page or email
-        us at info@ztech.ca
-      </p>
-      <p>Happy Holiday!</p>
-    </>
+    </div>
+
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     width: "100wv",
+    //     // justifyContent: "space-around",
+    //   }}
+    // >
+    //   <div style={{ width: "50%" }}>
+    //     <p>
+    //       Spin Hobby's design and dev teams are working to bring new tone to the
+    //       website. We expect to be back by the beginning of April 2023. We
+    //       apologize for inconvenience.
+    //     </p>
+    //     <p>Follow us on facebook to keep updated!</p>
+
+    //     <a href="https://www.facebook.com/profile.php?id=100084221636045">
+    //       <div style={{ width: "200px" }}>
+    //         <img
+    //           style={{ height: "auto", maxWidth: "100%" }}
+    //           src="/logo/external/facebook/FindUs-FB-RGB-1067.png"
+    //           alt="find us on facebook"
+    //         />
+    //       </div>
+    //     </a>
+    //     <div style={{ width: "100px" }}>
+    //       <img
+    //         style={{ height: "auto", maxWidth: "100%" }}
+    //         src="/QR/qrcode.svg"
+    //         alt="find us on facebook"
+    //       />
+    //     </div>
+    //     <p>
+    //       For all inquiries, please feel free to message on facebook page or
+    //       email us at info@ztech.ca
+    //     </p>
+    //     <p>Happy Holiday!</p>
+    //   </div>
+
+    //   <div style={{ width: "50%" }}>
+    //     <img
+    //       style={{ position: "absolute", height: "100vh" }}
+    //       alt="mascot"
+    //       src="assets/mascot.png"
+    //     />
+    //   </div>
+    // </div>
   );
 
   //return (
