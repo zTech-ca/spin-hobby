65acb5892dab1c4f405e7a215763731d7d32392f
Azusa Fukuda <afukuda@mySeneca.ca>
2023-08-07 15:20:15 -0400

    clear warnings

--- src/view/components/NavBar/Search.tsx
+++ src/view/components/NavBar/Search.tsx
@@ -6,6 +6,14 @@ interface Props {
   onNav?: boolean;
 }
 
+enum Category {
+  Volvo = "volvo",
+  Saab = "saab",
+  Mercedes = "mercedes",
+  Audi = "audi",
+  Toyota = "toyota",
+}
+
 export default function Search({ onNav = true }: Props) {
   const [search, setSearch] = useState<string>("");
   const [openCategoryList, setOpenCategoryList] = useState<boolean>(false);
@@ -30,7 +38,7 @@ export default function Search({ onNav = true }: Props) {
         className="navbar-search-category-toggler"
         onClick={handleOpenCategoryList}
       >
-        <div className="navbar-search-selected-category">All</div>
+        <div className="navbar-search-selected-category">{category}</div>
         {openCategoryList ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
         {openCategoryList && (
           <div className="dropdown-menu" style={{position: "absolute", zIndex: 100 }}>
@@ -46,21 +54,11 @@ export default function Search({ onNav = true }: Props) {
           onChange={handleSearchInput}
         />
       </form>
-      <div className="navbar-search-magnifier">
+      <div className="navbar-search-magnifier" >
         <FcSearch size={"1.5em"} />
       </div>
     </div>
   );
-}
-
-
-
-enum Category {
-  Volvo = "volvo",
-  Saab = "saab",
-  Mercedes = "mercedes",
-  Audi = "audi",
-  Toyota = "toyota",
 }
 
 function Dropdown({
@@ -74,7 +72,7 @@ function Dropdown({
       <div className="dropdown">
         <ul className="categories" id="categories" >
           {Object.values(Category).map((category, i) => (
-            <li key={i} value={category}>
+            <li key={i} value={category} >
               {category}
             </li>
           ))}
