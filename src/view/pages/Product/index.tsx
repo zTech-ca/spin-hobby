4f10cbeaea41abcbb0ed98d612ec3e491aa01f78
yuta123 <yuta.otsuki@gmail.com>
2023-10-03 11:14:35 -0700

    trying to pass deployment

--- src/view/pages/Product/index.tsx
+++ src/view/pages/Product/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { getProductData } from "../../../api";
-import ImageSlider from "./ImageSlider/ImageSlider";
 import ProductCard from "./ProductCard/ProductCard";
 
 export interface IProductData {}
