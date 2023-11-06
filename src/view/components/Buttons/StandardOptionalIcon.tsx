cd7cecdc105dd1f91d46321f26d257eef3057fcd
Viet Nhien Tran Duc <vtranduc@adobe.com>
2023-11-05 17:21:05 -0700

    password input

--- src/view/components/Buttons/StandardOptionalIcon.tsx
+++ src/view/components/Buttons/StandardOptionalIcon.tsx
@@ -3,13 +3,17 @@ import { IconType } from "react-icons";
 
 interface Props {
   icon?: IconType;
+  onClick?: () => void;
   text: string;
   classNames?: string;
 }
 
 export function StandardOptionalIcon(props: Props) {
   return (
-    <div className={`button-standard-optional-icon ${props.classNames || ""}`}>
+    <div
+      className={`button-standard-optional-icon ${props.classNames || ""}`}
+      onClick={props.onClick}
+    >
       {props.icon && (
         <div className="button-standard-optional-icon-icon">
           <props.icon size={35} />
--- src/view/modal/Login.tsx
+++ src/view/modal/Login.tsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { StandardOptionalIcon } from "../components/Buttons";
 import { BiEnvelope } from "react-icons/bi";
 import { Separator } from "view/components/Separator";
+import { InnerLabelInput } from "view/components/Inputs";
+
+interface LoginInputs {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const [inputs, setInputs] = useState<LoginInputs>({
+    email: "",
+    password: "",
+  });
+
+  function onSubmit() {
+    console.log("submitted!");
+  }
+
   return (
     <div className="modal-login">
       <div id="modal-login-header">Welcome Back</div>
-      <form id="modal-login-form">
-        <div className="modal-input-field">
-          <label>E-mail address</label>
-          <input title="email" name="email" type="email" />
-        </div>
-        <div className="modal-input-field">
-          <label>Password</label>
-          <input title="password" name="password" type="password" />
-        </div>
+      <form
+        id="modal-login-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+      >
+        <InnerLabelInput
+          classNames="modal-input-field"
+          label="E-mail address"
+          value={inputs.email}
+          onChange={(newVal) =>
+            setInputs((oldInputs) => ({ ...oldInputs, email: newVal }))
+          }
+        />
+        <InnerLabelInput
+          classNames="modal-input-field"
+          label="Password"
+          value={inputs.password}
+          onChange={(newVal) =>
+            setInputs((oldInputs) => ({ ...oldInputs, password: newVal }))
+          }
+          type="password"
+        />
         <StandardOptionalIcon
           classNames="modal-login-button-general"
           icon={BiEnvelope}
           text="Log in"
+          onClick={onSubmit}
         />
+        <input style={{ display: "none" }} type="submit" />
       </form>
-      <div id="modal-login-forget-password">Forget Password?</div>
+      <div id="modal-login-forget-password">Forgot Password?</div>
       <Separator classNames="modal-login-separator-1" text="OR" />
       <Separator
         classNames="modal-login-separator-1"
